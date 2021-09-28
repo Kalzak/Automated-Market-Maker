db4336b2a31fde09295e42e290ddec39db48c071
Kalzak <kalzakdev@protonmail.com>
2021-09-28 22:52:22 +0800

    Added and improved testing

--- test/testAmm.test.js
+++ test/testAmm.test.js
@@ -38,10 +38,18 @@ contract("Amm", (accounts) => {
 		assert.equal(newT2Address, t2Instance.address, "Address did not change");
 	});
 
+	it("getLiqidity returns correct values", async() => {
+		const ammInstance = await Amm.deployed();
+		let liq = await ammInstance.getLiquidity();
+		assert.equal("10000000000000000000", BigInt(liq[0],toString()), "Unexpected value returned");
+		assert.equal("1000000000000000000000", BigInt(liq[1],toString()), "Unexpected value returned");
+	});
+
 	it("calculateConstantProduct calculates correctly", async() => {
 		const ammInstance = await Amm.deployed();
 		await ammInstance.calculateConstantProduct();
 		let res = await ammInstance.constantProduct();
+		assert.equal("10000000000000000000000000000000000000000", res.toString());
 	});
 
 	it("quoteT1toT2 returns value", async() => {
@@ -54,17 +62,25 @@ contract("Amm", (accounts) => {
 		let res = await ammInstance.quoteT2_to_T1('100000000000000000000');
 	});
 
-	it("exchangeT1toT2 returns value", async() => {
+	it("exchangeT1toT2 changes holdings of amm", async() => {
 		const ammInstance = await Amm.deployed();
+		let liqBefore = await ammInstance.getLiquidity();
 		const t1Instance = await Token1.deployed();
 		await t1Instance.approve(ammInstance.address, '100000000000000000', {from: accounts[0]});
 		await ammInstance.exchangeT1_to_T2('100000000000000000', {from: accounts[0]});
+		let liqAfter = await ammInstance.getLiquidity();
+		assert.notEqual(liqBefore[0].toString(), liqAfter[0].toString(), "Liqidity did not change");
+		assert.notEqual(liqBefore[1].toString(), liqAfter[1].toString(), "Liqidity did not change");
 	});
 
-	it("exchangeT2_to_T1 returns value", async() => {
+	it("exchangeT2toT1 changes holdings of amm", async() => {
 		const ammInstance = await Amm.deployed();
+		let liqBefore = await ammInstance.getLiquidity();
 		const t2Instance = await Token2.deployed();
 		await t2Instance.approve(ammInstance.address, '10000000000000000000', {from: accounts[1]});
 		await ammInstance.exchangeT2_to_T1('10000000000000000000', {from: accounts[1]});
+		let liqAfter = await ammInstance.getLiquidity();
+		assert.notEqual(liqBefore[0].toString(), liqAfter[0].toString(), "Liqidity did not change");
+		assert.notEqual(liqBefore[1].toString(), liqAfter[1].toString(), "Liqidity did not change");
 	});
 });
